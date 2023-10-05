ed80f37f5defe94c9f7d9a4f2125fc63ead96dbd
Justin Murray <justin@popsql.com>
2023-10-05 14:53:23 -0600

    example of streaming not working for certain queries

--- README.md
+++ README.md
@@ -1,33 +1,11 @@
-# electron-duckdb
+# Streaming data out of DuckDB
 
-## Crashing Electron with DuckDB
+I'm working on building out a streaming interface for DuckDB that handles arbitrary SQL and streams back any results. I have something that works for "normal" selects that return rows of data, but I'm finding that it does not work as expected for what I'll call "scalar" queries, like `select 1` or `select count(*) from table`. I'm not sure if this is a bug or if I'm doing something wrong.
 
-In this repo, I'm highlighting what appears to be a bug in DuckDB when used with Electron. To reproduce:
-
-```bash
-npm install
-npm run test-electron
-```
-
-You should see the following error:
-
-```bash
-libc++abi: terminating due to uncaught exception of type Napi::Error: External buffers are not allowed
-/path/to/Electron exited with signal SIGABRT
-```
-
-I've traced the problem down to [this line](https://github.com/duckdb/duckdb/blob/7512d7ff4f5b8afd86f247335e5c4f75538bf43f/tools/nodejs/lib/duckdb.js#L143), where the `nextIpcBuffer()` call seems to be what is triggering the `External buffers are not allowed` error.
-
-You can then test that the same code executes find in a standard node environment:
+See the code comments for more explanation. You can run the test with:
 
 ```bash
 npm run test-node
 ```
 
-## Streaming data out of DuckDB
-
-In addition to the electron problems, even in node I'm having trouble streaming data out of DuckDB row by row. I expected passing the `arrowIPCStream` into an arrow `RecordBatchReader` would accomplish this. However, I only get the fields, and no row data. This can be observed by running:
-
-```bash
-npm run test-node
-```
+Note: the `test-electron` script crashes due to [duckdb#9172](https://github.com/duckdb/duckdb/issues/9172).
--- test.js
+++ test.js
@@ -1,12 +1,32 @@
 import duckdb from "duckdb";
 import { RecordBatchReader } from "apache-arrow";
 
+// Stream results by getting an arrowIPCStream, then iterating with an arrow RecordBatchReader
+const streamResults = async (con, sql) => {
+  console.log("\nstreamResults:", sql);
+  const results = [];
+  for await (const batch of await RecordBatchReader.from(
+    await con.arrowIPCStream(sql)
+  )) {
+    console.log(
+      `batch has ${batch.schema.fields.length} fields, ${batch.numRows} rows, ${batch.numCols} cols`
+    );
+    for (const row of batch) {
+      const result = {};
+      for (const [field, val] of row) {
+        result[field] = val;
+      }
+      results.push(result);
+    }
+  }
+  return results;
+};
+
 export const test = async () => {
-  const sql = "select * from generate_series(0, 5) as t(v)";
   const db = new duckdb.Database(":memory:");
   const con = db.connect();
 
-  // not sure if this is necessary, but load the arrow extension
+  // load the arrow extension
   await new Promise((resolve, reject) => {
     con.exec("INSTALL arrow; LOAD arrow;", (err, res) => {
       if (err) {
@@ -17,54 +37,30 @@ export const test = async () => {
     });
   });
 
-  // confirm that the extension is loaded
-  const ext = await new Promise((resolve, reject) => {
-    con.all(
-      "select extension_name FROM duckdb_extensions() where loaded = true and installed = true",
-      (err, res) => {
+  // This works correctly (as does any select of columns from a table)
+  console.log(
+    "working example:",
+    await streamResults(con, "select * from generate_series(0, 5) as t(v)")
+  );
+
+  // But any scalar query does not iterate any rows (where 1 is expected)
+  const sql = "select now() as t";
+  // const sql = "select count(*) from generate_series(0, 5)";
+  console.log("broken example:", await streamResults(con, sql));
+
+  // The same query works as expected with con.all
+  console.log(
+    "\ncon.all",
+    await new Promise((resolve, reject) => {
+      con.all(sql, (err, res) => {
         if (err) {
           reject(err);
         } else {
           resolve(res);
         }
-      }
-    );
-  });
-  console.log("extensions", ext);
-
-  // show that simple queries work
-  const result = await new Promise((resolve, reject) => {
-    con.all(sql, (err, res) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(res);
-      }
-    });
-  });
-  console.log("con.all", result);
-
-  // attempt to use an arrow stream
-  const stream = await con.arrowIPCStream(sql);
-
-  // traced the problem through the source to here: https://github.com/duckdb/duckdb/blob/7512d7ff4f5b8afd86f247335e5c4f75538bf43f/tools/nodejs/lib/duckdb.js#L143
-  // you can uncomment the following to see it crashes on this call (which happens in the iterator below)
-  // const rawBuffer = await stream.stream_result.nextIpcBuffer();
-  // console.log("rawBuffer", rawBuffer);
-
-  // Use an arrow RecordBatchReader to iterate over the stream
-  // on node, this executes without error
-  const reader = await RecordBatchReader.from(stream);
-  for await (const batch of reader) {
-    console.log(batch.schema.fields, batch.numRows, batch.numCols);
-    // however, we only get the fields, and no rows
-    for (const row of batch) {
-      console.log(row);
-      for (const [field, val] of row) {
-        console.log(field, val);
-      }
-    }
-  }
+      });
+    })
+  );
 
   console.log("done");
 };
